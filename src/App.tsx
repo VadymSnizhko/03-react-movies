3edc2888a9928eae535bb1e26c246a39323098ed
VadymSnizhko <snizhko@gmail.com>
2025-05-26 01:50:49 +0300

    next steps Home Work 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.module.css'
+import { useState } from "react";
+import toast from "react-hot-toast";
+import {type Movie } from "./types/movie";
+import { fetchMovies } from "./services/movieService";
+import SearchBar from "./components/SearchBar/SearchBar";
+import MovieGrid from "./components/MovieGrid/MovieGrid";
+import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import MovieModal from "./components/MovieModal/MovieModal";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleSearch = async (query: string) => {
+    if (!query.trim()) {
+      toast.error("Please enter your search query.");
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+      setHasError(false);
+      const results = await fetchMovies(query);
+      if (results.length === 0) {
+        toast.error("No movies found for your request.");
+      }
+      setMovies(results);
+    } catch (error) {
+      setHasError(true);
+      toast.error("Error fetching movies.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleSelectMovie = (movie: Movie) => {
+    //console.log("Selected movie:", movie);
+    setSelectedMovie(movie);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null);
+  };
 
   return (
     <>
-      <p>Home Work 03</p>
+      <SearchBar onSubmit={handleSearch} />
+      {isLoading && <Loader />}
+      {hasError && <ErrorMessage />}
+      {!isLoading && !hasError && movies.length > 0 && (
+        <MovieGrid movies={movies} onSelect={handleSelectMovie} />
+      )}
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,6 @@
+// src/components/Loader/Loader.tsx
+import css from "./Loader.module.css";
+
+export default function Loader() {
+  return <p className={css.text}>Loading movies, please wait...</p>;
+}
